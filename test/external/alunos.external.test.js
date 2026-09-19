73d68403511c0071b1839369dde8124387de85f4
Welington Elian Melo Steinmetz <105945762+welington007@users.noreply.github.com>
2026-09-18 21:11:41 -0300

    Enhance aluno registration test assertions
    Add assertions to validate the response body of the student registration.

--- test/external/alunos.external.test.js
+++ test/external/alunos.external.test.js
@@ -38,6 +38,9 @@ describe('Gestão de Alunos - Testes Externos', () => {
 
         // Validar que ele foi cadastrado
         expect(cadastroAlunoResposta.status).to.equal(201);
+        expect(cadastroAlunoResposta.body.nome).to.equal(caso.nome);
+        expect(cadastroAlunoResposta.body.email).to.equal(caso.email);
+        expect(cadastroAlunoResposta.body.matricula).to.equal(caso.matricula);       
         //expect(cadastroAlunoResposta.body.alunoId).to.equal(alunoId);
         //expect(cadastroAlunoResposta.body.nome).to.equal('Julio de Lima');
         //expect(cadastroAlunoResposta.body.email).to.equal('julio.lima@example.com');
